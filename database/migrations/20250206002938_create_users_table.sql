e744ec446fc2aac1c9a80030fb9249239224d726
Murage <25152892+0xMurage@users.noreply.github.com>
2025-02-06 03:52:36 +0300

    feat: add sample migrations

---
+++ database/migrations/20250206002938_create_users_table.sql
@@ -0,0 +1,13 @@
+-- migrate:up
+CREATE TABLE users
+(
+    id            uuid PRIMARY KEY         NOT NULL DEFAULT gen_random_uuid(),
+    firstname    TEXT,
+    lastname     TEXT,
+    email TEXT                     NOT NULL UNIQUE,
+    status TEXT NOT NULL CHECK ( status IN ('ACTIVE','SUSPENDED','INVITED')),
+    created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+-- migrate:down
+DROP TABLE users;

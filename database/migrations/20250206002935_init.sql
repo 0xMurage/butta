80f58d0a26f0fc13e6b3d69c848e51c67150af37
Murage <25152892+0xMurage@users.noreply.github.com>
2025-02-06 04:50:45 +0300

    chore: lint format

--- database/migrations/20250206002935_init.sql
+++ database/migrations/20250206002935_init.sql
@@ -1,9 +1,11 @@
 -- migrate:up
+CREATE
+EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
+SET
+intervalstyle = 'iso_8601'; -- Set time interval to be based on the ISO 8601 standard
 
-SET intervalstyle = 'iso_8601'; -- Set time interval to be based on the ISO 8601 standard
-
-SET TIMEZONE = 'UTC'; -- Set session's timezone to UTC 0
+SET
+TIMEZONE = 'UTC'; -- Set session's timezone to UTC 0
 
 -- migrate:down
